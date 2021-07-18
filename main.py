98301ff2bdd70ff23ce48374e790211920b4a6e5
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2021-07-18 12:29:26 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+
+import requests
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkalidns.request.v20150109 import (
+    DescribeDomainRecordsRequest,
+    UpdateDomainRecordRequest
+)
+
+
+class Config:
+    def __init__(self):
+        self.path = 'config.json'
+        self.default = {
+            'IPV6': True,
+            'AccessKeyID': 'AccessKeyID',
+            'AccessKeySecret': 'AccessKeySecret',
+            'Domain': 'Domain',
+            'RR': 'RR'
+        }
+        self.data = None
+        self._check()
+
+    def _check(self):
+        self._read()
+        save_flag = False
+        for key, value in self.default.items():
+            if key not in self.data.keys():
+                self.data[key] = value
+                save_flag = True
+        if save_flag:
+            self._save()
+
+    def _read(self):
+        if os.path.isfile(self.path):
+            with open(self.path) as f:
+                self.data = json.load(f)
+        else:
+            self.data = self.default
+            self._save()
+
+    def _save(self):
+        with open(self.path, 'w') as f:
+            json.dump(self.data, f, indent=4)
+
+    def get(self, key):
+        if key not in self.data.keys():
+            raise ValueError(key + ' is not in configuration')
+        else:
+            return self.data[key]
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+
+def get_ip(ipv6=False):
+    if ipv6:
+        return requests.get('https://api6.ipify.org/?format=json').json()['ip']
+    else:
+        return requests.get('https://api4.ipify.org/?format=json').json()['ip']
+
+
+def get_old_record(domain: str, client: AcsClient):
+    request = DescribeDomainRecordsRequest.DescribeDomainRecordsRequest()
+    request.set_accept_format('json')
+
+    request.set_DomainName(domain)
+
+    response = client.do_action_with_exception(request)
+    for i in json.loads(response)['DomainRecords']['Record']:
+        if i['RR'] == 'home':
+            return i
+
+
+def main():
+    config = Config()
+    client = AcsClient(config['AccessKeyID'], config['AccessKeySecret'])
+
+    old_record = get_old_record(config['Domain'], client)
+    ip = get_ip(config['IPV6'])
+    if old_record['Value'] == ip:
+        print('No changed')
+        return
+    request = UpdateDomainRecordRequest.UpdateDomainRecordRequest()
+    request.set_accept_format('json')
+
+    request.set_RR(config['RR'], )
+    request.set_RecordId(old_record['RecordId'])
+    request.set_Type('AAAA' if config['IPV6'] else 'A')
+    request.set_Value(ip)
+
+    print(json.loads(client.do_action_with_exception(request)))
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,39 @@
+# DDNS
+
+> 阿里云动态域名解析工具
+
+## 使用
+
+### 安装依赖
+
+```text
+pip install -r requirements.txt
+```
+
+### 启动
+
+```text
+python main.py
+```
+
+## 配置
+
+### IPV6
+
+是否使用 ipv6
+
+### AccessKeyID
+
+阿里云 AccessKeyID
+
+### AccessKeySecret
+
+阿里云 AccessKeySecret
+
+### Domain
+
+域名
+
+### RR
+
+主机记录
